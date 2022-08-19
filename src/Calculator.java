1c4183ac39983f5fc772ce7b4e4cdd7c394171e6
Ajinkya Sawant <sawant.ajinkya12345@gmail.com>
2022-08-19 15:49:02 +0530

    hard coded values

---
+++ src/Calculator.java
@@ -0,0 +1,32 @@
+
+
+class Calculator {
+  public static void main(String[] args) {
+
+    char operator;
+    Double number1, number2, result;
+
+
+  	 number1 = 9.0;
+
+ 	 number2 = 5.0;
+
+      
+
+        result = number1 + number2;
+        System.out.println(number1 + " + " + number2 + " = " + result);
+
+     
+
+        result = number1 - number2;
+        System.out.println(number1 + " - " + number2 + " = " + result);
+    
+
+
+        result = number1 * number2;
+        System.out.println(number1 + " * " + number2 + " = " + result);
+   
+     
+     
+  }
+}
