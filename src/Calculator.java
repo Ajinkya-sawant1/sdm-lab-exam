d96cf13eee829b1aa1857c1b95ff62861ce71405
Ajinkya Sawant <sawant.ajinkya12345@gmail.com>
2022-08-19 16:03:08 +0530

    user choice via switch case added

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 
 class Calculator {
   public static void main(String[] args) {
@@ -6,27 +6,51 @@ class Calculator {
     char operator;
     Double number1, number2, result;
 
+    Scanner input = new Scanner(System.in);
 
-  	 number1 = 9.0;
+  
+    System.out.println("Choose an operator: +, -, *, or /");
+    operator = input.next().charAt(0);
 
- 	 number2 = 5.0;
+  
+    System.out.println("Enter first number");
+    number1 = input.nextDouble();
+
+    System.out.println("Enter second number");
+    number2 = input.nextDouble();
+
+
+    switch (operator) {
 
       
-
+      case '+':
         result = number1 + number2;
         System.out.println(number1 + " + " + number2 + " = " + result);
+        break;
 
      
-
+      case '-':
         result = number1 - number2;
         System.out.println(number1 + " - " + number2 + " = " + result);
-    
+        break;
 
-
+     
+      case '*':
         result = number1 * number2;
         System.out.println(number1 + " * " + number2 + " = " + result);
-   
+        break;
+
      
-     
+      case '/':
+        result = number1 / number2;
+        System.out.println(number1 + " / " + number2 + " = " + result);
+        break;
+
+      default:
+        System.out.println("Invalid operator!");
+        break;
+    }
+
+    input.close();
   }
 }
